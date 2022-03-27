f93b9ecefd28a4d238f0a9238f6482bb40da58c2
adittia12 <si19.adittiaagustian@mhs.ubpkarawang.ac.id>
2022-03-27 20:38:36 +0700

    AppCardView

---
+++ app/src/main/java/com/example/cardview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.cardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CardView cardView = (CardView)findViewById(R.id.cv_main);
+        cardView.setCardElevation(0.5f);
+    }
+}
